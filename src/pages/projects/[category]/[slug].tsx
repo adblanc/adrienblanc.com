f9b1353b4abf803fac4c45944db7d319fa56e0ee
adblanc <ablanc@student.42.fr>
2021-03-03 21:33:39 +0100

    some design

--- src/pages/projects/[category]/[slug].tsx
+++ src/pages/projects/[category]/[slug].tsx
@@ -2,7 +2,7 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import hydrate from "next-mdx-remote/hydrate";
 import renderToString from "next-mdx-remote/render-to-string";
 import { NextSeo } from "next-seo";
-import Layout from "../../../components/Layout";
+import Link from "next/link";
 import {
   getAllProjectsPaths,
   getProjectData,
@@ -26,14 +26,34 @@ const Project = ({ project, source }: Props) => {
         description={project.description}
       />
       <div className="flex items-center justify-center">
-        <article className="prose lg:prose-lg">
-          <div>
-            <h1 className="text-center">{project.title}</h1>
-            <p className="text-center text-gray-500">
-              {project.tags.join(" | ")}
-            </p>
+        <article className="prose lg:prose-lg border border-gray-500 rounded-md ">
+          <div className="relative w-full h-full border-gray-600 border-2 rounded-md p-8 -top-2 left-2 bg-white">
+            <Link href="/">
+              <a>
+                <svg
+                  className="w-6 h-6 cursor-pointer"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                  />
+                </svg>
+              </a>
+            </Link>
+            <div>
+              <h1 className="text-center">{project.title}</h1>
+              <p className="text-center text-gray-500">
+                {project.tags.join(" | ")}
+              </p>
+            </div>
+            <div>{content}</div>
           </div>
-          <div>{content}</div>
         </article>
       </div>
     </>
