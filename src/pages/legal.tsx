fc1cb093cafe7b4242b9f0d9adf105bf030a8753
Adrien Blanc <adrien.blanc@blank.app>
2022-07-13 17:55:16 +0200

    feat(legal): update legal mention

---
+++ src/pages/legal.tsx
@@ -0,0 +1,31 @@
+
+
+const LegalPage = () => {
+	return (
+		<>
+		<p>
+			Ce site est édité par Monsieur Adrien BLANC, résidant 23 avenue de la Chapellerie, 94370 Sucy-en-Brie.
+			</p>
+			<p>
+			Téléphone : 06 31 07 34 26
+			</p>
+
+			<p>
+			Courrier électronique : adrien.blancdarsonval@gmail.com
+			</p>
+
+
+ 
+			<p>
+			Le directeur de la publication est Monsieur Adrien BLANC.
+			</p>
+
+	
+				<p>
+			Ce site est hébergé par la société <span className="font-semibold">Vercel Inc.</span>, située 340 S Lemon Ave #4133 Walnut, CA 91789, et joignable via <a className="hover:underline font-semibold" href="mailto:privacy@vercel.com">privacy@vercel.com</a>
+				</p>
+		</>
+	)
+}
+
+export default LegalPage
